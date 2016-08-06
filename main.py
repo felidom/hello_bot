b3091dd274651ca3823ee37c4d55405e2b5cf5cd
dominikfelix@yahoo.de <EU58uj69>
2016-08-06 12:20:13 +0200

    first

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import requests
 
 app = Flask(__name__)
 
-access_token = 'EAAW895WLezkBAGisDrIsZBuazNgk2Ll8fnpjbtg1wXOrJu3aYyFVLrCg3ZABUD3S70vEZAWHsbeYR4lKSm8mrbIdkZAimsAo9FHEHkbZCZBW89Wy56mYWlDyTmD5bzQAFSscMNkBJxbH7KfaVGgCZAo5gIin0SQeBmLRvRzdIstSwZDZD'
+access_token = 'EAAD8B3l0pWIBAKfsoUiqCOlAiPHuQvqwOiZBhjOfcu2PbrxTbOcvWWHHUYCez5FZBMupy9xVZAitkpmceUMFX1onR4fhcQf43BOZAho4ub0xxdpw8JIUxiqy3lJ79DLKzIZACbwJLfQiwKZB2vI5nqHWTrckX7tTB76m5cT2p53wZDZD'
 
 
 @app.route("/", methods=["GET"])
@@ -39,7 +39,7 @@ def post_webhook():
                         message_text = messaging_event['message']['text']
                         image = "http://cdn.shopify.com/s/files/1/0080/8372/products/tattly_jen_mussari_hello_script_web_design_01_grande.jpg"
                         element = create_generic_template_element("Hello", image, message_text)
-                        reply_with_generic_template(sender_id, element)
+                        reply_with_generic_template(sender_id, [element])
 
                         # do_rules(sender_id, message_text)
 
